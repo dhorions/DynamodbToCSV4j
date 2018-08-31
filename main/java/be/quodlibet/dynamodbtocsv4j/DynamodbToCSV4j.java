945ca2047f39d9822263848a05529d0049fe3fac
Anthony Milbourne <anthony.milbourne@mporium.com>
2018-08-31 15:25:07 +0100

    Added support for result paging
    Added support for large dumps which return paged results.
    Added support for projection expressions to determine which fields to return.
    Added .pom file to make build easier.
    Upgraded to latest AWS library.
    Other code refactors.

--- main/java/be/quodlibet/dynamodbtocsv4j/DynamodbToCSV4j.java
+++ main/java/be/quodlibet/dynamodbtocsv4j/DynamodbToCSV4j.java
@@ -1,7 +1,7 @@
 package be.quodlibet.dynamodbtocsv4j;
 
-import com.amazonaws.util.json.JSONException;
-import com.amazonaws.util.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
